98918b313492f44b4412aab1c5a05dfb0f503234
daisuke96 <tkd8926.daisuk@hotmail.co.jp>
2022-07-03 17:21:34 +0900

    インデックス指定のデータ取得ソースをコミット

--- fav-store-project/src/components/StoreList.vue
+++ fav-store-project/src/components/StoreList.vue
@@ -1,5 +1,27 @@
-<!---->
+
 <template>
+  <v-simple-table class="store-table">
+     <thead>
+        <tr>
+          お店一覧
+        </tr>
+      </thead>
+      <hr>
+      <tbody>
+        <tr
+          v-for="store in stores"
+          :key="store.Name"
+        >
+          <td>{{ store.Name }}<br><br>{{ store.Memo }}</td>
+        </tr>
+      </tbody>
+      <br>
+    <button @click="test('202205081708')">データ取得ボタン</button><br>
+    {{storesTest}}
+  </v-simple-table>
+
+
+<!--
   <div class="store-list">
   {{stores}}
     <ul>
@@ -9,22 +31,25 @@
     </ul>
     <ModalWind :val="postItem" v-show="showContent" @close="closeModal"/>
   </div>
+
+  -->
 </template>
 
 <script>
 import { getDatabase, ref, child, get } from 'firebase/database';
 import Vue from 'vue';
-import ModalWind from './ModalWind.vue'
+//import ModalWind from './ModalWind.vue'
 
 export default Vue.extend ({
   name: 'StoreList',
   components: {
-    ModalWind
+ //   ModalWind
   },
 
   data() {
     return {
       // 辞書型 ID:{店名等,メモ,...}の形で登録
+      storesTest: [],
       stores: [],
       item: true,
       showContent: false,
@@ -54,11 +79,40 @@ export default Vue.extend ({
     },
     closeModal () {
       this.showContent = false
+    },
+    test (storeNum) {
+      console.log(storeNum);
+      const db = getDatabase()
+      const dbRef = ref(db)
+      get(child(dbRef,'StoreList/'+storeNum)).then((snapshot) => {
+      if (snapshot.exists()) {
+        this.storesTest = snapshot.val()
+      } else {
+        console.log('No data available')
+      }
+    }).catch((error) => {
+      console.error(error)
+    })
     }
   }
 })
 </script>
 
 <style>
+.store-table{
+  width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+}
+
+.store-table thead{
+  text-align: center;
+  font-size: 15pt;
+}
+
+.store-table tbody{
+  height: 100px;
+}
 
 </style>
